066d8584310fbe3ec726bd7e4695388c58c4b071
Diana Parra Corbacho <dparracorbac@vmware.com>
2023-05-30 15:30:54 +0200

    Tests: wait for network partition to happen/recover instead of sleep

--- deps/rabbit/test/feature_flags_SUITE.erl
+++ deps/rabbit/test/feature_flags_SUITE.erl
@@ -13,6 +13,10 @@
 
 -include_lib("rabbit_common/include/logging.hrl").
 
+-import(clustering_utils, [
+                           assert_cluster_status/2
+                          ]).
+
 -export([suite/0,
          all/0,
          groups/0,
@@ -904,8 +908,8 @@ do_enable_feature_flag_when_ff_file_is_unwritable(Config) ->
 enable_feature_flag_with_a_network_partition(Config) ->
     FeatureName = ff_from_testsuite,
     ClusterSize = ?config(rmq_nodes_count, Config),
-    [A, B, C, D, E] = rabbit_ct_broker_helpers:get_node_configs(
-                        Config, nodename),
+    [A, B, C, D, E] = All = rabbit_ct_broker_helpers:get_node_configs(
+                              Config, nodename),
     True = lists:duplicate(ClusterSize, true),
     False = lists:duplicate(ClusterSize, false),
 
@@ -925,7 +929,10 @@ enable_feature_flag_with_a_network_partition(Config) ->
                  {E, C},
                  {E, D}],
     block(NodePairs),
-    timer:sleep(1000),
+
+    %% Wait for the network partition to happen
+    assert_cluster_status({All, [B, E]}, [B, E]),
+    assert_cluster_status({All, [A, C, D]}, [A, C, D]),
 
     %% Enabling the feature flag should fail in the specific case of
     %% `ff_from_testsuite', if the network is broken.
@@ -938,11 +945,11 @@ enable_feature_flag_with_a_network_partition(Config) ->
 
     %% Repair the network and try again to enable the feature flag.
     unblock(NodePairs),
-    timer:sleep(10000),
     [?assertEqual(ok, rabbit_ct_broker_helpers:stop_node(Config, N))
      || N <- [A, C, D]],
     [?assertEqual(ok, rabbit_ct_broker_helpers:start_node(Config, N))
      || N <- [A, C, D]],
+    assert_cluster_status({All, All}, All),
     declare_arbitrary_feature_flag(Config),
 
     %% Enabling the feature flag works.
@@ -978,7 +985,8 @@ mark_feature_flag_as_enabled_with_a_network_partition(Config) ->
                  {B, D},
                  {B, E}],
     block(NodePairs),
-    timer:sleep(1000),
+    assert_cluster_status({AllNodes, [B]}, [B]),
+    assert_cluster_status({AllNodes, [A, C, D, E]}, [A, C, D, E]),
 
     %% Mark the feature flag as enabled on all nodes from node B. This
     %% is expected to timeout.
