db22569caafbba75050b9d884ff1b1917d389dc8
David Ansari <david.ansari@gmx.de>
2023-06-13 15:32:07 +0000

    Remove erlang:port_command/2 hack
    as selective receives are efficient in OTP 26:
    ```
    OTP-18431
    Application(s):
    compiler, stdlib
    Related Id(s):
    PR-6739
    Improved the selective receive optimization, which can now be enabled for references returned from other functions.
    This greatly improves the performance of gen_server:send_request/3, gen_server:wait_response/2, and similar functions.
    ```

--- deps/rabbitmq_mqtt/src/rabbit_mqtt_reader.erl
+++ deps/rabbitmq_mqtt/src/rabbit_mqtt_reader.erl
@@ -184,12 +184,6 @@ handle_info({Tag, Sock, Reason}, State = #state{socket = Sock})
             when Tag =:= tcp_error; Tag =:= ssl_error ->
     network_error(Reason, State);
 
-handle_info({inet_reply, Sock, ok}, State = #state{socket = Sock}) ->
-    {noreply, State, ?HIBERNATE_AFTER};
-
-handle_info({inet_reply, Sock, {error, Reason}}, State = #state{socket = Sock}) ->
-    network_error(Reason, State);
-
 handle_info({conserve_resources, Conserve}, State) ->
     maybe_process_deferred_recv(
         control_throttle(State #state{ conserve = Conserve }));
@@ -325,14 +319,16 @@ process_received_bytes(Bytes, State = #state{socket = Socket,
             case ProcState of
                 connect_packet_unprocessed ->
                     Send = fun(Data) ->
-                                   try rabbit_net:port_command(Socket, Data)
-                                   catch error:Error ->
-                                             ?LOG_ERROR("writing to MQTT socket ~p failed: ~p",
-                                                        [Socket, Error])
-                                   end,
-                                   ok
+                                   case rabbit_net:send(Socket, Data) of
+                                       ok ->
+                                           ok;
+                                       {error, Reason} ->
+                                           ?LOG_ERROR("writing to MQTT socket ~p failed: ~p",
+                                                      [Socket, Reason]),
+                                           exit({send_failed, Reason})
+                                   end
                            end,
-                    case rabbit_mqtt_processor:init(Packet, Socket, ConnName, Send) of
+                    try rabbit_mqtt_processor:init(Packet, Socket, ConnName, Send) of
                         {ok, ProcState1} ->
                             ?LOG_INFO("Accepted MQTT connection ~ts for client ID ~ts",
                                       [ConnName, rabbit_mqtt_processor:info(client_id, ProcState1)]),
@@ -348,9 +344,11 @@ process_received_bytes(Bytes, State = #state{socket = Socket,
                             ?LOG_ERROR("Rejected MQTT connection ~ts with CONNACK return code ~p",
                                        [ConnName, ConnAckReturnCode]),
                             {stop, shutdown, {_SendWill = false, State}}
+                    catch exit:{send_failed, Reason} ->
+                              network_error(Reason, State)
                     end;
                 _ ->
-                    case rabbit_mqtt_processor:process_packet(Packet, ProcState) of
+                    try rabbit_mqtt_processor:process_packet(Packet, ProcState) of
                         {ok, ProcState1} ->
                             process_received_bytes(
                               Rest,
@@ -364,6 +362,8 @@ process_received_bytes(Bytes, State = #state{socket = Socket,
                             {stop, {shutdown, Reason}, pstate(State, ProcState1)};
                         {stop, disconnect, ProcState1} ->
                             {stop, normal, {_SendWill = false, pstate(State, ProcState1)}}
+                    catch exit:{send_failed, Reason} ->
+                              network_error(Reason, State)
                     end
             end;
         {error, {cannot_parse, Reason, Stacktrace}} ->
